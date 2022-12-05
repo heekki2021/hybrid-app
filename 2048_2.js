7de14cc6999263c36397337bc95c0cc115a85d88
heekki2021 <kfri1999@gmail.com>
2022-12-06 00:58:06 +0900

    2048 day1

---
+++ 2048_2.js
@@ -0,0 +1,33 @@
+function keylog(e) {
+  console.log(e);
+}
+
+const fbyf = [4][4];
+
+class block {
+  constructor() {
+    this.value = 0;
+    this.color = "black";
+    this.Bwidth = 50;
+    this.Bheigh = 50;
+
+    const rand1 = Math.floor(math.rand1);
+    const rand2 = Math.floor(math.rand2);
+
+    // fbyf[rand1][rand2] = ;
+
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = value;
+    newDiv.style.width = this.Bwidth;
+    newDiv.style.height = this.Bheigh;
+    newDiv.style.color = this.color;
+  }
+
+  createBlock() {
+    const newDiv = document.createElement("div");
+  }
+}
+
+class makeBlock {}
+
+var b1 = new block();
